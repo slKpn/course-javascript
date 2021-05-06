4eae9b16227fab84cd7cafcad94b1d5496f7cfe6
Pavel Kozhin <kozhin.p@mail.ru>
2021-05-06 21:03:58 +0500

    вторая неделя - 3 задание

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -16,7 +16,24 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
-function isAllTrue(array, fn) {}
+function isAllTrue(array, fn) {
+ 
+      if(Array.isArray(array)==false || array.length == false){
+        throw new Error('empty array');
+      }
+      if(typeof(fn) != 'function' ){
+        throw new Error('fn is not a function');
+      }
+
+
+  for(const elementArr of array){
+    if(fn(elementArr) == false){
+      return false
+    }
+  }
+  return true;
+
+}
 
 /*
  Задание 2:
@@ -34,7 +51,23 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  if(Array.isArray(array)==false || array.length == false){
+    throw new Error('empty array');
+  }
+  if(typeof(fn) != 'function' ){
+    throw new Error('fn is not a function');
+  }
+
+
+  for(const elementArr of array){
+    if(fn(elementArr) == true){
+      return true
+    }
+  }
+return false;
+
+}
 
 /*
  Задание 3:
@@ -47,7 +80,26 @@ function isSomeTrue(array, fn) {}
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn, ...args) {}
+function returnBadArguments(fn, ...args) {
+  let arrErrorArg = [];
+
+  if(typeof(fn) != 'function' ){
+    throw new Error('fn is not a function');
+  }
+
+
+  
+
+  for(const argsFn of args){
+    try{
+      fn(argsFn);
+    } catch{
+      arrErrorArg.push(argsFn);
+    }
+  }
+
+  return arrErrorArg;
+}
 
 /*
  Задание 4:
@@ -66,7 +118,53 @@ function returnBadArguments(fn, ...args) {}
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator(number = 0) {}
+function calculator(number = 0) {
+  if(Number.isFinite(number) == false){
+    throw new Error('number is not a number');
+  }
+
+  return {
+    sum(...args){
+      let result = number;
+      
+      for(let argSum of args){
+        result += argSum;
+      }
+
+      return result;
+    },
+    dif(...args){
+      let result = number;
+      
+      for(let argDif of args){
+        result -= argDif;
+      }
+
+      return result;
+    },
+    div(...args){
+      let result = number;
+      
+      for(let argDiv of args){
+        if(argDiv == 0){
+          throw new Error('division by 0');
+        }
+        result /= argDiv;
+      }
+
+      return result;
+    },
+    mul(...args){
+      let result = number;
+      
+      for(let argMul of args){
+        result *= argMul;
+      }
+
+      return result;
+    }
+  };
+}
 
 /* При решении задач, постарайтесь использовать отладчик */
 
